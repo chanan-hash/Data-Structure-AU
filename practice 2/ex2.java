a88409ae2b169d98e8c39550f2901f729d7196bc
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-03-13 16:11:10 +0200

    Deleting test

--- practice 2/ex2.java
+++ practice 2/ex2.java
@@ -4,6 +4,8 @@ public class ex2 {
         double a = 5;
         double b = 6;
         System.out.println(sum(a, b));
+        System.out.println(subtract(a, b));
+
     }
 
     // Functions for ex1
@@ -16,4 +18,15 @@ public class ex2 {
         }
         return sum(a+1, b-1); // in each recursion we're stepping forward 'a', and taking 'b' one step back
     }
+
+    // Subtraction - according to this recursion formula "a - b = (a - (b-1)) + 1  , a - 0 = a"
+    public static double subtract(double a, double b){
+        if(b==0){
+            return a;
+        }
+        return subtract(a-1,b-1); // against to the sum, on sub, both a and b are been reduced by 1
+    }
+
+    //Multiplication - according to this recursion formula "according to this recursion formula"
+
 }
