151ffa0a7fdeeff2f32d5dbd9e652a176cc161d8
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-03-14 11:15:29 +0200

    Adding ex3 to ex2

--- practice 2/ex2.java
+++ practice 2/ex2.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class ex2 {
 
     public static void main(String[] args) {
@@ -14,6 +16,15 @@ public class ex2 {
         System.out.println(remainder(12, 6.5));
         System.out.println(remainder(28, 4));
         System.out.println(remainder(4, 28));
+
+        System.out.println(pow2(4));
+        System.out.println(pow2(0));
+        System.out.println(pow2(1));
+        System.out.println(pow2(2));
+        System.out.println(pow2(3));
+
+        System.out.println(reverse(314));
+
     }
 
     // Functions for ex1
@@ -24,7 +35,7 @@ public class ex2 {
         if (b == 0) {
             return a;
         }
-        return sum(a + 1, b - 1); // in each recursion we're stepping forward 'a', and taking 'b' one step back
+        return sum(a + 1, b - 1); // in each - recursion we're stepping forward 'a', and taking 'b' one step back
     }
 
     // Subtraction - according to this recursion formula "a - b = (a - (b-1)) + 1  , a - 0 = a"
@@ -55,10 +66,34 @@ public class ex2 {
 
     // Remainder - according to this recursion formula "a%b = (a-b)%b , a&b = a, a<b"
     public static double remainder(double a, double b) {
-        if (a<b){
+        if (a < b) {
             return a;
         }
-        return remainder(a-b,b);
+        return remainder(a - b, b);
+    }
+
+
+    // Ex2
+    // Recursion of 2 power --> 2^n = 2^(n-1) + 2^(n-1)
+    // Do it without multiplication
+    public static int pow2(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return pow2(n - 1) + pow2(n - 1);
+    }
+
+    //Ex3
+    // Reversing a number in iterative way
+    // Actually it is giving us a new number that it is the reverse number
+    // n % 10 = giving us the One's digit
+    public static int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = (rev *10) + n%10 ;
+            n /= 10;
+        }
+        return rev;
     }
 
 }
--- practice 2/ex2Test.java
+++ practice 2/ex2Test.java
@@ -10,18 +10,29 @@ class ex2Test {
         double b = 6;
         double sum = ex2.sum(a, b);
         double sub = ex2.subtract(a, b);
-        double mul = ex2.mul(2.5,2);
-        double div = ex2.div(10,2);
+        double mul = ex2.mul(2.5, 2);
+        double div = ex2.div(10, 2);
         assertEquals(11, sum);
         assertEquals(-1, sub);
-        assertEquals(0,ex2.subtract(a,a));
-        assertEquals(5,mul);
-        assertEquals(5,div);
+        assertEquals(0, ex2.subtract(a, a));
+        assertEquals(5, mul);
+        assertEquals(5, div);
 
-        assertEquals(6,ex2.remainder(6,12));
-        assertEquals(5.5,ex2.remainder(12,6.5));
-        assertEquals(0,ex2.remainder(28,4));
-        assertEquals(4,ex2.remainder(4,28));
+        assertEquals(6, ex2.remainder(6, 12));
+        assertEquals(5.5, ex2.remainder(12, 6.5));
+        assertEquals(0, ex2.remainder(28, 4));
+        assertEquals(4, ex2.remainder(4, 28));
+
+        assertEquals(1, ex2.pow2(0));
+        assertEquals(2, ex2.pow2(1));
+        assertEquals(4, ex2.pow2(2));
+        assertEquals(8, ex2.pow2(3));
+        assertEquals(16, ex2.pow2(4));
+        assertEquals(32, ex2.pow2(5));
+        assertEquals(64, ex2.pow2(6));
+
+        assertEquals(1234, ex2.reverse(4321));
+
 
     }
 
