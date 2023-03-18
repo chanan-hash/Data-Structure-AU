4dcb19692e01b030298af1115ff9bc743aa584bb
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-03-19 01:34:41 +0200

    adding ex3 to ex3

--- practice 3/ex3.java
+++ practice 3/ex3.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ex3 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 8, 9, 11, 57};
         System.out.println(binarySreach(arr, 10));
         System.out.println(recBinaryserach(arr, 11, arr[0], arr.length - 1));
-        System.out.println(finalRecBin(arr,57));
-        System.out.println(finalRecBin(arr,12));
+        System.out.println(finalRecBin(arr, 57));
+        System.out.println(finalRecBin(arr, 12));
 
+
+        String[] srr = {"contribute", "geeks", "ide", "practice"};
+        String x = "ide";
+        int result = stringBinarySerach(srr, x);
+        System.out.println(result);
 
     }
 
@@ -46,8 +54,8 @@ public class ex3 {
     }
 
     // Comfortable recursive binary search, using the function above.
-    public static int finalRecBin(int[] arr, int value){
-        return recBinaryserach(arr,value,arr[0],arr.length-1);
+    public static int finalRecBin(int[] arr, int value) {
+        return recBinaryserach(arr, value, arr[0], arr.length - 1);
     }
 
     // Ex2 is to make binary search to a game
@@ -55,4 +63,41 @@ public class ex3 {
 
     // Ex3 --> if the binary search is different from -1, we will add them to array list and return id to array
     // we need to create before a binary search for String
+
+    public static int stringBinarySerach(String[] srr, String str) {
+        int L = 0;
+        int R = srr.length - 1;
+
+        while (L < R) {
+            int mid = (R + L) / 2;
+            int res = str.compareTo(srr[mid]); // a for loop of O(n) --> but comparing to string by a comperator
+            if (res == 0) {
+                return mid;
+            } else if (res < 0) { //we are comparing the chars
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static String[] joinSrr(String[] s1, String[] s2, String[] s3) {
+        ArrayList<String> srr = new ArrayList<>();
+        // we can run and find the min array but it more work for the program
+        // int minLength = Math.min(Math.min(s1.length,s2.length),s3.length); // running on the minimum array
+        //String sArr = // the minimum String array
+        for (String str : s1) { // for reach loop
+            if ((stringBinarySerach(s2, str) != -1) && (stringBinarySerach(s3, str) != -1)) { // means the string from s1 is in s2 and s3
+                srr.add(str);
+            }
+        }
+        // return (String[]) srr.toArray(); //need to be cast
+         return srr.toArray(new String[]{});
+    }
+
+    // Ex4
+
+
+
 }
--- practice 3/ex3Test.java
+++ practice 3/ex3Test.java
@@ -4,12 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ex3Test {
     @Test
-    public void testBinarySearch(){
+    public void testBinarySearch() {
         int[] arr = {1, 2, 3, 4, 5, 8, 9, 11, 57};
-        assertEquals(-1,ex3.binarySreach(arr,10));
-        assertEquals(7,ex3.recBinaryserach(arr,11,arr[0],arr.length-1));
-        assertEquals(5,ex3.finalRecBin(arr,8));
-        assertEquals(-1,ex3.finalRecBin(arr,12));
+        String[] srr = {"contribute", "geeks", "ide", "practice"};
+        String x = "ide";
+        assertEquals(-1, ex3.binarySreach(arr, 10));
+        assertEquals(7, ex3.recBinaryserach(arr, 11, arr[0], arr.length - 1));
+        assertEquals(5, ex3.finalRecBin(arr, 8));
+        assertEquals(-1, ex3.finalRecBin(arr, 12));
+
+        assertEquals(2, ex3.stringBinarySerach(srr, x));
+        assertEquals(-1, ex3.stringBinarySerach(srr, "abc"));
 
     }
+
+    void testCommonElements() {
+        String[] arr1 = {"apple", "banana", "orange", "grape"};
+        String[] arr2 = {"banana", "grape", "kiwi", "pear"};
+        String[] arr3 = {"banana", "grape", "kiwi", "orange", "strawberry"};
+        String[] expected = {"banana", "grape"};
+        String[] actual = ex3.joinSrr(arr1, arr2, arr3);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testEmptyArrays() {
+        String[] arr1 = {};
+        String[] arr2 = {"banana", "kiwi", "grape", "pear"};
+        String[] arr3 = {};
+
+        String[] expected = {};
+        String[] actual = ex3.joinSrr(arr1, arr2, arr3);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testNoCommonElements() {
+        String[] arr1 = {"apple", "banana", "orange"};
+        String[] arr2 = {"kiwi", "pear"};
+        String[] arr3 = {"strawberry", "watermelon"};
+
+        String[] expected = {};
+        String[] actual = ex3.joinSrr(arr1, arr2, arr3);
+
+        assertArrayEquals(expected, actual);
+    }
+
+
 }
