a57b41642469130b24da2bb08be4d28194666807
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-03-22 16:00:32 +0200

    adding foo3

--- Ex1/Ex1.java
+++ Ex1/Ex1.java
@@ -3,9 +3,12 @@ public class Ex1 {
     public static void main(String[] args) {
 //        foo1(10);
 //        System.out.println();
-        foo2(6);
+//        foo2(6);
+        foo3(17);
     }
 
+
+    // Complexity O(nlog(n))
     public static void foo1(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = i; j > 0; j = j / 2) {
@@ -22,6 +25,14 @@ public class Ex1 {
         }
         for (int j = sum; j > 0; j = j - n) {
             System.out.println(j);
+        }
+    }
+
+    public static void foo3(int n) {
+        for (int i = 1; i < n; i = i * 2) {
+            for (int j = 1; j < i; j++) {
+                System.out.println(i);
+            }
         }
     }
 }
