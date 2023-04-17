d5ea99cc078e8a7fdaf64f81f06fac75074ce5ea
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-04-17 23:49:19 +0300

    Biulding queue structure

---
+++ datatStructures/MyQueue.java
@@ -0,0 +1,74 @@
+public class MyQueue {
+    // implementing queue with cycled array, means that the next value in the array is calculated by the formula-->
+    // i = (i + 1) % n, when 'i' is the index, and 'n' is the size of the array.
+    // array that calculated like this called cycled array
+
+    // variables
+    private int front = 0, tail = 0; // pointers for the queue, to decrease the complexity
+    private int size = 0; // the amount of numbers that actually in the array
+    private int maxSize; // The memory allocation of the array in the memory --> the max size of the array
+    private Integer[] data; // the array will be represented as an object of integers
+
+    // constructor
+    public MyQueue(int max) { // creating new queue, and initializing the class variable
+        this.maxSize = max;
+        this.front = 0;
+        this.tail = 0;
+        this.size = 0;
+        data = new Integer[this.maxSize];
+    }
+
+
+    // functions
+    public boolean enqueue(Integer value) {
+        boolean ans = true;
+        if (size == maxSize) {
+            ans = false;
+            System.out.println("The queue is full");
+        } else { // if tail == n-1 --> tail = 0
+            data[tail] = value; // putting the new value in the end of the queue
+            tail = (tail + 1) % maxSize; // formula for tail advancement of the tail is --> tail = (tail+1) % n, when 'n' is the size of the array
+            size++;
+        }
+        return ans;
+    }
+
+
+    // giving showing the first element, but not removing it
+    public Integer peek() {
+        Integer ans = null;
+        if (isEmpty()) {
+            System.out.println("The queue is empty");
+            ans = null;
+        } else {
+            ans = data[front];
+        }
+        return ans;
+    }
+
+    public boolean contains(Integer val) {
+        boolean res = false;
+        for (int i = 0; i < size; i++) {
+            if (val == data[i + front % maxSize]) {
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void clear() {
+        size = 0;
+        front = 0;
+        this.tail = 0;
+    }
+
+
+}
