dc26edfbe1a6988330a0368739610db18da6a9f8
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-04-18 11:28:16 +0300

    Building and addnig functions generic queue

---
+++ datatStructures/GenQueue.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+
+public class GenQueue<T> {
+    /**
+     * This class represents generic queue.
+     * Unlike generic stack that we've implemented by ArrayList,
+     * The generic queue we'll implement by LinkedList, because of the 'front' and 'tail'
+     * that it is easier to get them by front = getFirst, tail = getLast.
+     * the complexity is O(1), because we have Nodes/pointer for then, and in array list it can be some time O(n)
+     */
+
+    // Variables
+    int maxLength = -1; // to initialize the max length of the queue, the allocation in the memory
+    LinkedList<T> queue = new LinkedList<>();
+
+    // constructors
+    public GenQueue(int maxSize) {
+        this.maxLength = maxSize;
+        queue = new LinkedList<T>();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public boolean enqueue(T valueType) {
+        boolean res = true;
+        if (queue.size() == maxLength) {
+            res = false;
+            System.out.println("The queue is full");
+
+        } else { // if we had only 'if' statement, the code would continue and the 'res' variable would have changes
+            queue.offer(valueType); // adding at the last place the new value. if we are getting inside the class of java bt Ctrl + mouse clicking,
+            // we'll see the function is O(1)
+        }
+        return res;
+    }
+
+    // removing the first element from the queue --> FIFO
+    public T dequeue() {
+        T type = null;
+        if (queue.isEmpty()) {
+            System.out.println("The queue is empty");
+        } else {
+            type = queue.poll(); // poll function removing the first element from the LinkedList, and returning it.
+            // so we are keeping it in type, so we'll return it, and that how we know it worked
+            // poll --> because it's removing the first element in linked list, its is taking O(1)
+        }
+        return type;
+    }
+}
--- datatStructures/MyQueue.java
+++ datatStructures/MyQueue.java
@@ -1,5 +1,6 @@
 public class MyQueue {
-    /** implementing queue with cycled array, means that the next value in the array is calculated by the formula-->
+    /**
+     * implementing queue with cycled array, means that the next value in the array is calculated by the formula-->
      * i = (i + 1) % n, when 'i' is the index, and 'n' is the size of the array.
      * array that calculated like this called cycled array
      * In the cycled queue, we allocate the memory at the beginning and that it, we cant put values in the queue more than the maxSize
@@ -7,7 +8,6 @@ public class MyQueue {
      * The first way to implement queue is to keep the first index and last index, each time.
      * The problem when we want to add or to remove value we need to move also all the queue values O(n)
      * but the queue will be dynamic, and we don't need to allocate in the beginning place in the memory for the mexSize8
-     *
      */
 
     // variables
@@ -27,6 +27,7 @@ public class MyQueue {
 
 
     // functions
+    // O(1)
     public boolean enqueue(Integer value) {
         boolean ans = true;
         if (size == maxSize) {
@@ -43,6 +44,7 @@ public class MyQueue {
 
     // when we are using dequeue, after it finished we are returning the value we've removed.
     // that how we know the function succeeded
+    // O(1)
     public Integer dequeue() {
         Integer ans;
 
@@ -58,6 +60,7 @@ public class MyQueue {
     }
 
     // giving showing the first element, but not removing it
+    // O(1)
     public Integer peek() {
         Integer ans = null;
         if (isEmpty()) {
@@ -69,6 +72,7 @@ public class MyQueue {
         return ans;
     }
 
+    //O(n)
     public boolean contains(Integer val) {
         boolean res = false;
         for (int i = 0; i < size; i++) {
@@ -79,19 +83,21 @@ public class MyQueue {
         return res;
     }
 
+    // O(1)
     public boolean isEmpty() {
         return size == 0;
     }
 
+    // O(1)
     public int getSize() {
         return size;
     }
 
+    // O(1)
     public void clear() {
         size = 0;
         front = 0;
         this.tail = 0;
     }
-
 
 }
