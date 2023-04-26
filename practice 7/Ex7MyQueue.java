9999c10cbe65498df37a1af88f6bb73ad4a60fe0
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-04-26 22:08:37 +0300

    Adding Ex1 - Ex3, to Ex7

--- practice 7/Ex7MyQueue.java
+++ practice 7/Ex7MyQueue.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class Ex7MyQueue {
 
     private int[] que;
@@ -29,4 +31,75 @@ public class Ex7MyQueue {
         return que[tail]; // we are updating the size so the the next time we'll dequeue, we will go back according to the formula
         // and returning the value in that place
     }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    // Returning if there no more place to add values
+    public boolean isfull() {
+        return size == que.length;
+    }
+
+    // Ex1
+    // O(size)
+    public String toString() {
+        if (size == 0) {
+            return "Queue is empty!";
+        } else {
+            String str = "[";
+            for (int i = 0; i < size; i++) {
+                str += ", " + que[i];
+            }
+            str += " ]";
+            return str;
+        }
+    }
+
+    // another way of doing that:
+    // for this we need pointer for first, and pointer for last, in this class
+    // from Eyal's levi github --> https://github.com/LeviEyal/DataStructuresCourse/blob/master/leead_exsercises/src/%D7%AA%D7%99%D7%A8%D7%92%D7%95%D7%9C%D7%99%D7%9D/MyQueue.java
+//    public String toString2() {
+//        if (size == 0) return "The Queue is empty";
+//        int k = first;
+//        String s = "The queue: [";
+//        for (int i = 0; i < size - 1; i++) {
+//            k = (first + i) % a.length;
+//            s += a[k] + ", ";
+//        }
+//        return s + "" + a[last] + "]";
+//    }
+
+    // Ex2
+    public boolean contains(int value) {
+        boolean ans = false;
+        for (int i = 0; i < size; i++) {
+            if (que[(pointer + i) % que.length] == value) { // we can also do it as regular searching
+                // because this formula is for cycled array, we'll go over the whole elements
+                ans = true;
+            }
+        }
+        return ans;
+    }
+
+    //Ex3
+    // first option is ro go ovr with a loop, but than it will be O(n), but this is not a linked list, we've implemented the Queue by an array
+    // because we're working with array we can just return the element in the que[index]
+    // and in cycled array we can return the que[(pointer + index) % que.length]
+    // and it making it as an O(1)
+    public int elementAt(int index) { // it can be parallel to "get(i)"
+        if (index >= size || index < 0) {
+            // return Integer.MAX_VALUE; // the index is bigger thean the size of the queue
+            throw new NoSuchElementException("the index is to bigger form the size of the queue");
+        } else {
+            // return que[index];
+            return que[(pointer + index) % que.length];
+        }
+    }
+    
 }
