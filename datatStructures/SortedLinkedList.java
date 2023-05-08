86dfe7020a2f0e6fc08fede4c7108523965f41fb
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-05-08 17:27:33 +0300

    adding soorted linked list

--- datatStructures/SortedLinkedList.java
+++ datatStructures/SortedLinkedList.java
@@ -50,22 +50,71 @@ public class SortedLinkedList {
         return ans;
     }
 
-    public String remove(String str){ //O(n)
-
+    public String remove(String str) { //O(n)
+        String ans = null;
+        if (head == null) { // empty list
+            return null;
+        } else if (head.data.equals(str)) { // removing the head
+            ans = head.data;
+            head = head.next; // and the head node will be thrown to the garbage collector
+        } else {
+            SortedNode prev = head, n = head; // One running One step, and the second with One before him
+            while (n.next != null && !n.data.equals(str) && n.data.compareTo(str) < 0) { //means we didn't fount the element, and it is not equal to the data, and also still small
+                prev = n;
+                n = n.next;
+            }
+            if (n.data.equals(str)) {
+                ans = n.data;
+                prev.next = n.next; // skipping on 'n' node and throwing it to thw garbage collector
+                size--;
+            }
+        }
+        return ans;
     }
 
 
-
-    public String toString(){
+    public String toString() {
         String s = "[";
-        if (head != null){
+        if (head != null) {
             s = s + head.toString() + ", ";
-            for (SortedNode n = head.next; n != null; n=n.next){
+            for (SortedNode n = head.next; n != null; n = n.next) {
                 s = s + n.toString() + ", ";
             }
-            s = s.substring(0, s.length()-2); // to take of some of the spaces
+            s = s.substring(0, s.length() - 2); // to take of some of the spaces
         }
-        return s+"]";
+        return s + "]";
     }
 
+
+    public static void main(String[] args) {
+        SortedLinkedList list = new SortedLinkedList();
+        list.add("f");
+        list.add("t");
+        list.add("d");
+        list.add("m");
+        list.add("b");
+        list.add("u");
+        System.out.println("list: " + list);
+
+        System.out.println("a: " + list.contains("a"));
+        System.out.println("b: " + list.contains("b"));
+        System.out.println("t: " + list.contains("t"));
+        System.out.println("m: " + list.contains("m"));
+        System.out.println("remove x: " + list.remove("x"));
+        System.out.println(list);
+        System.out.println("remove b: " + list.remove("b"));
+        System.out.println(list);
+        System.out.println("remove m: " + list.remove("m"));
+        System.out.println(list);
+        System.out.println("remove t: " + list.remove("t"));
+        System.out.println(list);
+        System.out.println("remove d: " + list.remove("d"));
+        System.out.println(list);
+        System.out.println("remove f: " + list.remove("f"));
+        System.out.println(list);
+        System.out.println("remove d: " + list.remove("d"));
+        System.out.println(list);
+    }
 }
+
+
