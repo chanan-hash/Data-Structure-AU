e67b04fef6c035c178214f487fe7e4fe6b64a6d9
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-05-12 08:39:04 +0300

    Starting practice 9

--- datatStructures/Black_RedTree.java
+++
@@ -1,4 +0,0 @@
-public class Black_RedTree {
-
-//    private class
-}
---
+++ practice 9/ex9.java
@@ -0,0 +1,26 @@
+public class ex9 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    // Ex1
+    //
+    /* preorder:
+     *      tree1. 3,1,14,5,4,7,16,15,20
+     *      tree2. F,B,A,D,C,E,G,I,H
+     *      tree3. F.B,A,C,F,E,G
+     *
+     * inorder:
+     *      tree1 = 1,3,3,4,7,14,15,16,20
+     *      tree2 = A,B,C,D,E,F,G.H.I
+     *      tree3 = A,B,C,D,E,F,G
+     *
+     * postorder:
+     *      tree1 -->
+     *      tree2 -->
+     *      tree3 -->
+     */
+}
