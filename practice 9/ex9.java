934625364e6ca2b8315d31164c7c5f5e6cd15cee
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-05-12 16:38:08 +0300

    need to separate the classes

--- practice 9/ex9.java
+++ practice 9/ex9.java
@@ -1,44 +1,40 @@
 public class ex9 {
-
-
     public static void main(String[] args) {
+            BT_BST.BST tree1 = new BT_BST.BST();
+            BT_BST.BST tree2 = new BT_BST.BST();
+            BT_BST.BST tree3 = new BT_BST.BST();
 
-    }
+            tree1.insert(3);
+            tree1.insert(14);
+            tree1.insert(1);
+            tree1.insert(5);
+            tree1.insert(4);
+            tree1.insert(7);
+            tree1.insert(16);
+            tree1.insert(15);
+            tree1.insert(20);
 
-    private class Node {
-        int data;
-        Node left, right, parent;
-
-        public Node(int num) {
-            this.data = num;
-            left = right = null;
+            tree1.printPreOrder();
+            tree1.printInOrder();
+            tree1.printPostOrder();
         }
+        // Ex1
+        //
+        /* preorder:
+         *      tree1. 3,1,14,5,4,7,16,15,20
+         *      tree2. F,B,A,D,C,E,G,I,H
+         *      tree3. F.B,A,C,F,E,G
+         *
+         * inorder:
+         *      tree1 = 1,3,3,4,7,14,15,16,20
+         *      tree2 = A,B,C,D,E,F,G.H.I
+         *      tree3 = A,B,C,D,E,F,G
+         *
+         * postorder:
+         *      tree1 --> 1,4,7,5,15,20,16,14,3
+         *      tree2 --> A,C,E,D,B,H,I,G,F
+         *      tree3 --> A,C,B,E,G,F,D
+         */
 
-        public Node(int num, Node left, Node right) {
-            this.data = num;
-            this.left = left;
-            this.right = right;
-        }
     }
-
-    public class BT{
-
-    }
-    // Ex1
-    //
-    /* preorder:
-     *      tree1. 3,1,14,5,4,7,16,15,20
-     *      tree2. F,B,A,D,C,E,G,I,H
-     *      tree3. F.B,A,C,F,E,G
-     *
-     * inorder:
-     *      tree1 = 1,3,3,4,7,14,15,16,20
-     *      tree2 = A,B,C,D,E,F,G.H.I
-     *      tree3 = A,B,C,D,E,F,G
-     *
-     * postorder:
-     *      tree1 --> 1,4,7,5,15,20,16,14,3
-     *      tree2 --> A,C,E,D,B,H,I,G,F
-     *      tree3 --> A,C,B,E,G,F,D
-     */
 }
