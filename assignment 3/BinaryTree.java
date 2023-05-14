3281c98c22e4795a2571f588b1ce23170e251821
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-05-14 19:55:24 +0300

    Adding documentation to ex1

--- assignment 3/BinaryTree.java
+++ assignment 3/BinaryTree.java
@@ -24,46 +24,50 @@ public class BinaryTree {
      * we need to go over the whole tree and check it, so The complexity is O(n).
      * <p>
      * There are 2 way do it:
-     * 1. put the whole tree in Arraylist by inorder, because One of the binary tree qualities is that inorder we will get it sorted.
-     * then we can go over the array and check if it sorted, if not return false
-     * <p>
+     * 1.
+     *  put the whole tree in Arraylist by inorder, because One of the binary tree qualities is that inorder we will get it sorted.
+     *  then we can go over the array and check if it sorted, if not return false
+     *
      * 2.
+     *  The function gets Node(the root), maximum value and minimum value.
+     *  if the Node is null, means the tree is a search tree.
+     *      explanation: in the recursion if we got to that value that is null and didn't return false till there,
+     *      means the tree is built like a search tree
+     *  Then if the max value (that in the recursion will represent the right side of the tree), is less or equal to the node key, means it is not a search tree,
+     *  because we'll get the nodes value is bigger than it's right son. (or smaller than it's left son)
+     *  The same for the min value, that in the recursion will represent the left side of the tree.
+     *  In each recursive call we are passing the Nodes value as the min value to the left subtree,
+     *  and as the max value to the right subtree
+     *
      */
-//    public boolean isValidBST(){
-//        if(root == null){
-//            return false; // the tree is empty
-//        }
-//        else if (!(root.left.getKey().compareTo(root.getKey()) < 0 && root.right.getKey().compareTo(root.getKey()) > 0 )){
-//
-//        }
-//        boolean ans = true;
-//        return ans;
-//    }
+
+    // visualisation for the function:
+    // https://www.youtube.com/watch?v=RzagTQQbn5k
     public boolean isValidBST() {
         return isValidBST(root, null, null);
     }
 
-
     // From assignment 3
-    public static boolean isValidBST(Node root, Integer max, Integer min) {
+    public static boolean isValidBST(Node n, Integer max, Integer min) {
 
         // an empty binary trees is a valid BST.
-        if (root == null) {
+        if (n == null) {
             return true;
         }
 
         // in the recursion when we go each to left or subtree it will check thew condition of the definition of
-        if (max != null && root.getKey() >= max) {
-            return false; // for checking the lef side
+        if (max != null && n.getKey() >= max) {
+            return false; // for checking the right side
         }
 
-        if (min != null && root.getKey() <= min) { // for checking the right side
+        if (min != null && n.getKey() <= min) { // for checking the left side
             return false;
         }
 
-        return isValidBST(root.left, root.getKey(), min) &&
-                isValidBST(root.right, max, root.getKey());
+        return isValidBST(n.left, n.getKey(), min) && isValidBST(n.right, max, n.getKey());
     }
+
+
 
     public boolean isBlackHeight() {
         boolean ans = true;
