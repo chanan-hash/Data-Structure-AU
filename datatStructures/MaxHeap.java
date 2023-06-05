798ee2ba5ce8e4853580c2251f6d23b0a8dcfab5
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-06-05 23:43:31 +0300

    creating MaxHeap

---
+++ datatStructures/MaxHeap.java
@@ -0,0 +1,20 @@
+public class MaxHeap {
+
+    /**
+     * This class is based on the code from the class that Leead showed us
+     * The heap tree is created from simple array
+     */
+
+    private int[] a;
+    private int size = 0;
+
+
+
+    // creating a new array with the capacity
+    public MaxHeap(int capacity) {
+        a = new int[capacity+1]; // ignore a[0]
+    }
+
+
+
+}
